cf7129065206420bdb60c82c5df24cedef81495d
lenin.tarrillo <70543160+lenintarrillo@users.noreply.github.com>
2021-09-23 02:09:19 -0500

    update cuytoken.sol
    restricción de acceso a los métodos de pago
    se agrego métodos de transferencias condicionadas.

--- cuyToken.sol
+++ cuyToken.sol
@@ -42,40 +42,28 @@ contract owned {
         admins[account] = false;    
     }
 }
-/**
- * The purpose of Whitelist contract is manage whitelist
- * Only accounts that are whitelisted will be able to hold cuytokens
- */
-contract Whitelist is owned {
-    mapping(address => bool) whitelist;
-    
-    function addWhitelist(address account) onlyAdmin public{
-        require(account != address(0) && !whitelist[account]);             
-        whitelist[account] = true;    
-    }
-    function isWhitelist(address account) public view returns (bool) {   
-        return whitelist[account];
-    }
-    function removeWhitelisted(address account) public onlyAdmin {
-        require(account != address(0) && whitelist[account]);
-        whitelist[account] = false;    
-    }
-}
+
+
+
+
+
+
 
 
 /**
- * The purpose of Loan contract is manage Lending system
+ * The purpose of CriptoCredit contract is manage Lending system
  * add debtors and the information of the loan that is granted to them
  */
 contract CriptoCredit is owned {
     mapping(address => LoanInfo) debtors;
 
-     string public constant LOAN_PAID_ERROR = "LOAN_ALREADY_PAID";
+     string public constant LOAN_PAID_ERROR = "CLIENTE_HAS_NO_LOAN_TO_PAY";
      string public constant LOAN_NOT_PAID_ERROR = "CLIENT_HAS_AN_UNPAID_LOAN";
      uint8 public constant LOAN_PAID_CODE = 0;
      uint8 public constant LOAN_NOT_PAID_CODE = 1;
     
     struct LoanInfo{
+        address scc;
         string idClient;
         string idBusiness;
         uint256 amountCuy;
@@ -109,16 +97,16 @@ modifier loanStatus (address account, bool status, uint8 verificationCode ) {
     }
     
 /**
- * This method records the information of a loan in the blockchain
+ * This method records the loan details
+ * only the crypto credit system (admin) can execute this method
  */
     function loanAdd(address account,string memory idClient,string memory idBusiness, uint256 amountCuy, uint256 amountFiat, uint256 interest ) onlyAdmin loanStatus(account, false,LOAN_NOT_PAID_CODE) internal{
         require(account != address(0));    
-        debtors[account] = LoanInfo(idClient,idBusiness,amountCuy,amountFiat,interest,amountFiat*interest,amountFiat + (amountFiat*interest), amountCuy,true );
+        debtors[account] = LoanInfo(msg.sender,idClient,idBusiness,amountCuy,amountFiat,interest,amountFiat*interest,amountFiat + (amountFiat*interest), amountCuy,true );
         
     }
     
    
-    
     
     function loanBalance(address account) public view returns (LoanInfo memory) {   
         return debtors[account];
@@ -127,14 +115,17 @@ modifier loanStatus (address account, bool status, uint8 verificationCode ) {
 /**
  * This method records the payment of a loan fee, previously verifying that the loan is still pending payment
  */
-    function loanPay(address account,uint256 amountFiat,uint256 amountCuy) internal loanStatus(account, true,LOAN_PAID_CODE) {
+    function loanPay(address account,uint256 amountFiat,uint256 amountCuy) onlyAdmin  loanStatus(account, true,LOAN_PAID_CODE) internal returns (bool)  {
         require(account != address(0));
         debtors[account].balanceFiat = debtors[account].balanceFiat - amountFiat; 
         debtors[account].balanceCuy = debtors[account].balanceCuy - amountCuy; 
-        if(debtors[account].balanceFiat>=debtors[account].amountFiat)
+        if(debtors[account].balanceFiat<=0)
         {
+            //the client completed the payment of his credit
             debtors[account].open=false;
         }
+        
+        return true;
         
     }
 }
@@ -193,11 +184,16 @@ interface IERC20 {
  *@notice The cuytoken implements the ERC20 token
  *@author Lenin Tarrillo (lenin.tarrillo.v@gmail.com)
  */
-contract CuyToken is IERC20, Whitelist, Pausable, CriptoCredit {
+contract CuyToken is IERC20, Pausable, CriptoCredit {
     TokenSummary public tokenSummary;
     mapping(address => uint256) internal balances;
+    mapping(address => uint256) internal conditionedBalances; //conditioned Balances
    
     mapping (address => mapping (address => uint256)) internal allowed;
+    mapping (address => mapping (address => uint256)) internal wlCBalances; //White list of conditioned Balances
+    
+    
+    
     uint256 public _totalSupply;
     
     
@@ -205,9 +201,16 @@ contract CuyToken is IERC20, Whitelist, Pausable, CriptoCredit {
     string public constant SUCCESS_MESSAGE = "SUCCESS";
     uint8 public constant NON_WHITELIST_CODE = 1;
     string public constant NON_WHITELIST_ERROR = "ILLEGAL_TRANSFER_TO_NON_WHITELISTED_ADDRESS";
+    string public constant INSUFFICIENT_FUNDS = "ILLEGAL_TRANSFER_INSUFFICIENT_FUNDS";
+    string public constant ILLEGAL_PAYMENT = "ILLEGAL_PAYMENT_STORE_NOT_ALLOWED";
     string public constant ZERO_CUYS_PAY = "ILLEGAL_ATTEMPT_TO_PAY_ZERO_CUYS";
     event Burn(address from, uint256 value);
     event Lend(address from, uint256 value);
+    event Paid(address from, uint256 value);
+    
+     event BalancesTransform(address sender,address from, uint256 value);
+    
+    
     
     struct TokenSummary {
         address initialAccount;
@@ -217,7 +220,6 @@ contract CuyToken is IERC20, Whitelist, Pausable, CriptoCredit {
         
     }
     constructor(string memory _name, string memory  _symbol, address _initialAccount, uint _initialBalance)  {
-        addWhitelist(_initialAccount);
         balances[_initialAccount] = _initialBalance;
         _totalSupply = _initialBalance;
         tokenSummary = TokenSummary(_initialAccount, _name, _symbol,18);
@@ -238,18 +240,8 @@ contract CuyToken is IERC20, Whitelist, Pausable, CriptoCredit {
         return  tokenSummary.decimals;
     }
 
-    modifier verify (address from, address to, uint256 value) {
-        uint8 restrictionCode = validateTransferRestricted(to);
-        require(restrictionCode == SUCCESS_CODE, messageHandler(restrictionCode));
-        _;
-    }
-    function validateTransferRestricted (address to) public view returns (uint8 restrictionCode) {
-        if (!isWhitelist(to)) {
-            restrictionCode = NON_WHITELIST_CODE;
-        } else {
-            restrictionCode = SUCCESS_CODE;
-        }
-    }
+   
+   
     function messageHandler (uint8 restrictionCode) public pure returns (string memory message) {
         if (restrictionCode == SUCCESS_CODE) {
             message = SUCCESS_MESSAGE;
@@ -261,17 +253,48 @@ contract CuyToken is IERC20, Whitelist, Pausable, CriptoCredit {
     function balanceOf(address account) public  view override returns (uint256) {
       return balances[account];
     }
-    function transfer (address to, uint256 value) public override verify(msg.sender, to, value)
+    
+    
+    function balanceConditionedOf(address account) public  view  returns (uint256) {
+      return conditionedBalances[account];
+    }
+    
+    
+    function isWhiteList(address account) public  view  returns (uint256) {
+      return wlCBalances[msg.sender][account];
+    }
+   
+   
+   
+    function transfer (address to, uint256 value) public override 
     whenNotPaused  returns (bool success) {
-        require(to != address(0) && balances[msg.sender]> value);
+        require(to != address(0) && (balances[msg.sender] - conditionedBalances[msg.sender])>= value, INSUFFICIENT_FUNDS);
         balances[msg.sender] = balances[msg.sender] - value;
         balances[to] = balances[to] + value;
         emit Transfer(msg.sender, to, value);
         return true;
     }
     
-    function transferFrom(address from, address spender,uint256 value) public override  verify(from, spender, value) whenNotPaused returns (bool) {
-        require(spender != address(0) && value <= balances[from] && value <= allowed[from][msg.sender]);
+    
+/**
+  * This method makes a transfer in the form of payment to an authorized store, 
+  * the cuytokens that will be spent will only be those that are marked as conditional balances
+ */
+     function shopPay(address to, uint256 value) public 
+    whenNotPaused  returns (bool success) {
+        require(to != address(0) && conditionedBalances[msg.sender]>= value, INSUFFICIENT_FUNDS);
+        require(wlCBalances[msg.sender][to]>= value, ILLEGAL_PAYMENT);
+        conditionedBalances[msg.sender] = conditionedBalances[msg.sender] - value;
+        wlCBalances[msg.sender][to]=wlCBalances[msg.sender][to] - value;
+         balances[to] = balances[to] + value;
+        emit Transfer(msg.sender, to, value);
+        return true;
+    }
+    
+    
+    
+    function transferFrom(address from, address spender,uint256 value) public override   whenNotPaused returns (bool) {
+        require(spender != address(0) && value <= (balances[from] - conditionedBalances[from] ) && value <= allowed[from][msg.sender], INSUFFICIENT_FUNDS );
         balances[from] = balances[from] - value;
         balances[spender] = balances[spender] + value;
         allowed[from][msg.sender] = allowed[from][msg.sender] - value;
@@ -281,12 +304,16 @@ contract CuyToken is IERC20, Whitelist, Pausable, CriptoCredit {
   function allowance(address _owner,address spender) public  view override returns (uint256) {
     return allowed[_owner][spender];
   }
+  
+  
+  
   function approve(address spender, uint256 value) public override returns (bool) {
         require(spender != address(0));
         allowed[msg.sender][spender] = value;
         emit Approval(msg.sender, spender, value);
         return true;
    }
+   
   function burn(uint256 value) public whenNotPaused  returns (bool success) {
     require(balances[msg.sender] >= value); 
     balances[msg.sender] -= value; 
@@ -296,19 +323,76 @@ contract CuyToken is IERC20, Whitelist, Pausable, CriptoCredit {
  }
  
  
+ 
+ /**
+  * This method transfers cuytokens and establishes spending restrictions
+  * cuytokens with restrictions can only be used to pay in stores
+ */
+  function transferConditioned (address to, uint256 value, address[] memory whitelist) public 
+    whenNotPaused  returns (bool success) {
+        require(to != address(0) && balances[msg.sender]> value);
+        balances[msg.sender] = balances[msg.sender] - value;
+        balances[to] = balances[to] + value;
+        conditionedBalances[to] = conditionedBalances[to] + value;
+        
+        for(uint i; i<whitelist.length; i++)
+        {
+            wlCBalances[to][whitelist[i]] =  wlCBalances[to][whitelist[i]] + value;
+        }
+            
+        
+        emit Transfer(msg.sender, to, value);
+        return true;
+    }
+    
+    
+    
+    
+ /**
+  * This method free from restrictions the conditioned balance of an account
+  * they can only be called by administrators. off chain should have sustained the change
+ */
+  function balancesTransform(address from, uint256 value) onlyAdmin public 
+    whenNotPaused  returns (bool success) {
+        require(from != address(0) && balances[from]> value);
+        
+        conditionedBalances[from] = conditionedBalances[from] - value;
+        
+        emit BalancesTransform(msg.sender, from, value);
+        return true;
+    }
+    
+ 
+ 
+ 
+ /**
+ *This method mines the cuytokens  and registers the crypto credit
+ *This method can only be used by the crypto credit system
+ *
+ */
+ 
  function lend(address account,string memory idClient,string memory idBusiness, uint256 amountCuy, uint256 amountFiat, uint256 interest )  public whenNotPaused onlyAdmin returns (bool){
         require(account != address(0)); 
         
-        mint(msg.sender,  amountCuy);
-        loanAdd(account,  idClient,  idBusiness,  amountCuy,  amountFiat,  interest );
+        if(mint(msg.sender,  amountCuy))//mined
+        {
+            
+        loanAdd(account,  idClient,  idBusiness,  amountCuy,  amountFiat,  interest ); //Lend
         emit Lend(account,amountCuy);
         return true;
+            
+        }
+        
+        return false;
+        
+       
         
     }
     
-
-
-    
+/**
+ *This method mines the cuytokens  
+ *
+ */    
   function mint(address account, uint256 value) internal whenNotPaused onlyAdmin returns (bool) {
     require(account != address(0));
     _totalSupply += value;
@@ -318,15 +402,24 @@ contract CuyToken is IERC20, Whitelist, Pausable, CriptoCredit {
       
   }
   
-  function Pay(address account,uint256 amountFiat,uint256 amountCuy) internal  {
+  
+  
+  /**
+ *This method records the total or partial payment of the crypto credit
+ */
+  function creditPay(address account,uint256 amountFiat,uint256 amountCuy) onlyAdmin internal returns (bool) {
         require(account != address(0) && amountCuy>0, ZERO_CUYS_PAY );
         
-        loanPay(account, amountFiat, amountCuy);
-        burn(amountCuy);
-        
+        if(loanPay(account, amountFiat, amountCuy))//Record the payment
+        {
+            burn(amountCuy); //Burn cuytokens
+            emit Paid(account,amountCuy);
+            
+            return true;
+        }
+        return false;
     }
-    
-    
+
    
   
 }
