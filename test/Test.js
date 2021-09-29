d7ced9cebc92a7c60d4ff013cd99e75ab0954323
Steve <lee.marreros@pucp.pe>
2021-09-28 20:22:42 -0500

    Adds tests for 'transfer' function

--- test/Test.js
+++ test/Test.js
@@ -7,6 +7,7 @@ const LOAN_PAID_ERROR = "CLIENTE_HAS_NO_LOAN_TO_PAY";
 const LOAN_NOT_PAID_ERROR = "CLIENT_HAS_AN_UNPAID_LOAN";
 const LOAN_PAID_CODE = 0;
 const LOAN_NOT_PAID_CODE = 1;
+let zero_address = "0x0000000000000000000000000000000000000000";
 
 contract("Pausable", (accounts) => {
   let accountOwner, Alice, Bob, Carlos, Damian, Evert;
@@ -45,7 +46,7 @@ contract("Pausable", (accounts) => {
   });
 });
 
-contract("cuyToken", (accounts) => {
+contract("cuyToken - Deploy, lend, mint and pay", (accounts) => {
   let accountOwner, Alice, Bob, Carlos, Damian, Evert;
   accountOwner = accounts[0];
   Alice = accounts[1];
@@ -64,7 +65,6 @@ contract("cuyToken", (accounts) => {
   let symbol = "CTK";
   let initialBalance = 0;
   let decimals = 18;
-  let zero_address = "0x0000000000000000000000000000000000000000";
   let cuyToken;
 
   beforeEach(async () => {
@@ -104,7 +104,7 @@ contract("cuyToken", (accounts) => {
     });
   });
 
-  describe("MINADO, PRÉSTAMO y PAGO DE PRÉSTAMO ", () => {
+  xdescribe("MINADO, PRÉSTAMO y PAGO DE PRÉSTAMO ", () => {
     // Datos del que recibirá el préstamo (loan)
     let account = Alice;
     let idClient = String(Math.floor(Math.random() * 10000)); // número de 5 dígitos
@@ -349,6 +349,126 @@ contract("cuyToken", (accounts) => {
       expect(amountFiat_).to.be.eq(String(amountFiat));
       expect(interest_).to.be.eq(String(interest));
       expect(balanceCuy).to.be.eq(String(amountCuy));
+      expect(balanceCuy).to.be.eq(String(amountCuy));
+      assert.equal(open, true, "For this account, 'open' is not set to true");
     });
+  });
+});
+
+contract("cuyToken - TOKEN transference", (accounts) => {
+  let accountOwner, Alice, Bob, Carlos, Damian, Evert;
+  accountOwner = accounts[0];
+  Alice = accounts[1];
+  Bob = accounts[2];
+  Carlos = accounts[3];
+  Damian = accounts[4];
+  Evert = accounts[5];
+  Fucci = accounts[6];
+  seventhAccount = accounts[7];
+  eighthAccount = accounts[8];
+  ninethAccount = accounts[9];
+  tenthAccount = accounts[10];
+  eleventhAccount = accounts[11];
+
+  beforeEach(async () => {
+    cuyToken = await CuyToken.deployed();
+  });
+
+  describe("Function 'transfer' - tranfiere a poseedores de tokens", () => {
+    let account = Alice;
+    let idClient = String(Math.floor(Math.random() * 10000)); // número de 5 dígitos
+    let idBusiness = String(Math.floor(Math.random() * 10000)); // número de 5 dígitos
+    let amountCuy = 30000;
+    let amountFiat = 10000;
+    let interest = 100;
+
+    it("No debería ser ejecutado cuando 'cuyToken' está PAUSADO", async () => {
+      await cuyToken.pause({ from: accountOwner });
+
+      // Bob es el comprador de tokens
+      await truffleAssert.reverts(
+        cuyToken.transfer(Bob, 20000, { from: accountOwner }),
+        "Contract is paused and cannot execute any operation."
+      );
+
+      await cuyToken.unpause({ from: accountOwner });
+    });
+
+    it("Cuenta destino no debería ser address(0)", async () => {
+      await truffleAssert.reverts(
+        cuyToken.transfer(zero_address, 20000, { from: accountOwner }),
+        "Cuenta de destino no debería ser address(0)"
+      );
+    });
+
+    it("Cuando no hay suficientes fondos, muestra mensaje apropiado", async () => {
+      await truffleAssert.reverts(
+        cuyToken.transfer(Carlos, 50000, { from: accountOwner }),
+        "ILLEGAL_TRANSFER_INSUFFICIENT_FUNDS"
+      );
+    });
+
+    it("Tranfiere cantidad exacta de tokens a comprador", async () => {
+      await cuyToken.lend(
+        account, // prestatario - no recibe los tokens. los recibe accountOwner
+        idClient,
+        idBusiness,
+        amountCuy,
+        amountFiat,
+        interest,
+        { from: accountOwner }
+      );
+      let totalSupplyBefore = await cuyToken.totalSupply();
+
+      let tokensToTransfer = 20000;
+      let txTransfer = await cuyToken.transfer(Carlos, tokensToTransfer, {
+        from: accountOwner,
+      });
+
+      let amountTokenPurchaser = await cuyToken.balanceOf(Carlos);
+      let amountTokensSystem = await cuyToken.balanceOf(accountOwner);
+      let totalSupply = await cuyToken.totalSupply();
+
+      String(Number(amountTokensSystem.toString()) + tokensToTransfer),
+        assert.equal(
+          amountTokenPurchaser.toString(),
+          String(tokensToTransfer),
+          "Cantidad recibida de tokens no concuerda con la transferida"
+        );
+
+      assert.equal(
+        amountTokensSystem.toString(),
+        String(Number(totalSupply.toString()) - tokensToTransfer),
+        "El admin no terminó con la catidad correcta de tokens luego de la transferencia"
+      );
+
+      assert.equal(
+        totalSupplyBefore.toString(),
+        totalSupply.toString(),
+        "El Suministro Total de tokens no debe variar luego de una transferencia."
+      );
+
+      assert.equal(
+        txTransfer.logs[0].event,
+        "Transfer",
+        "Evento 'Transfer' no fue llamado correctamente."
+      );
+      assert.equal(
+        txTransfer.logs[0].args._from,
+        accountOwner,
+        "Tokens no fueron transferidos desde un 'onlyAdmin'."
+      );
+      assert.equal(
+        txTransfer.logs[0].args._to,
+        Carlos,
+        "El que recibió los tokens no es el destinatario correcto"
+      );
+      assert.equal(
+        txTransfer.logs[0].args._value.toString(),
+        String(tokensToTransfer),
+        "Cantidad de tokens transferidas no fue el planteado"
+      );
+    });
+
   });
 });
